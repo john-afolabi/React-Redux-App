b7b80221653eb5242e6e915e9d7975005c3990b6
John Afolabi <johnafolabi.da@gmail.com>
2019-12-12 14:01:41 +0100

    Wrap app component in Redux state

--- rick-and-morty/src/index.js
+++ rick-and-morty/src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import thunk from "redux-thunk";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { charactersReducer } from "./state/reducers";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  charactersReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
